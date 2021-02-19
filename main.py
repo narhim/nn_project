cfac44eb646d7e3adf4e10e8ee039b8f868c0a7d
PDas2206 <priyanka.das.2206@gmail.com>
2021-02-19 18:22:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #Import modules
 import re
+import csv
 
 #Read Conll file:
 with open('sample.conll',"r",encoding = "utf-8") as file:
@@ -22,3 +23,49 @@ with open('sample.conll',"r",encoding = "utf-8") as file:
 with open('sample.tsv','w',encoding="utf-8") as file:
 	for line in corpus:
 		file.write(line)
+
+#Write tsv file
+with open('sample.tsv','r',encoding="utf-8") as tsvfile:
+    tsvreader = csv.reader(tsvfile, delimiter="\t")
+    maximum=0
+    minimum=0
+    mean=0
+    count_seq=0
+   
+    temp=0
+    pos_dict={}
+    pos_lost=[]
+    for line in tsvreader:
+        if line[0]!="*":
+            if int(line[0])>maximum:
+                maximum=int(line[0])
+                if count_seq==0:
+                    minimum=maximum
+            temp=int(line[0]) 
+            
+            # creating dictionary of pos
+            
+            if line[-1] not in pos_dict.keys():
+                pos_dict[line[-1]]=1
+            else:
+                pos_dict[line[-1]]+=1
+        else:
+            count_seq+=1
+            mean+=temp
+            if temp<minimum:
+                minimum=temp
+        
+    
+for k in pos_dict:
+    pos_dict[k]=pos_dict[k]/sum(pos_dict.values())
+
+#writing these info into a file
+with open("sample.info","w",encoding="utf-8") as f:
+    print("Max sequence length: ",maximum,file=f)
+    print("Min sequence length: ",minimum,file=f)
+    print("Mean sequence length: ",mean,file=f)
+    print("Number of sequences: ",count_seq,file=f)
+    print("Tags: ",file=f)
+    for k,v in pos_dict.items():
+        print(k+"\t"+str(v),file=f)
+    
